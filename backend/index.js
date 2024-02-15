e5bb4ea68960ac122a15381e011cfc4e780acdec
onin sama <ninoabao@gmail.com>
2024-02-15 22:09:21 +0800

    Change the structure of the project

---
+++ backend/index.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const cors = require("cors");
+const path = require('path');
+
+const {TaalReadings, TaalAccounts, db, addDoc, getDocs, query, orderBy, limit, where,startAt, endAt} = require("./config.js");
+const { getDoc, doc } = require("firebase/firestore");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'../frontend/index.html'));
+});
+
+const PORT = 5000;
+
+// api
+app.post("/api/taal/create", async(req, res)=>{
+    try {
+        const data = req.body;
+        // console.log(data);
+        await addDoc(TaalReadings, data);
+        res.send({msg:"Added Taal readings"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({msg: "An error occurred"});
+    }
+})
+
+app.post("/api/taal/readings", async (req, res) => {
+  try {
+      const { startDate, endDate } = req.body
+      const q = query(
+        TaalReadings,
+        orderBy("DateTime"),
+        where("DateTime", ">=", startDate),
+        where("DateTime", "<=", endDate)
+      )
+      const querySnapshot = await getDocs(q);
+
+      const docs = querySnapshot.docs.map(doc => doc.data())
+      res.send({ msg: "Retrieved Taal readings", data: docs })
+  } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "An error occurred" })
+  }
+});
+
+// get all accounts
+app.post("/api/taal/accounts", async (req, res) => {
+  try {
+    const {username, password} = req.body
+    const querySnapshot = await getDocs(query(TaalAccounts, where("Username", "==", username), where("Password","==",password)));
+    const docs = querySnapshot.docs.map(doc => doc.data());
+    docs.length>0?res.send({"AccFound": true}):res.send({"AccFound": false})
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ msg: "An error occurred", error: error.message });
+  }
+});
+
+app.post("/api/taal/createaccount", async (req, res) => {
+  try {
+    const data = req.body;
+    if (!data || !data.Username) {
+      res.status(400).send({ msg: "Invalid request data" });
+      return;
+    }
+
+    const snapshot = await getDocs(query(TaalAccounts, where("Username", "==", data.Username)));
+    if (!snapshot.empty) {
+      res.status(200).send({ msg: "Username already exists" })
+      return;
+    }
+    await addDoc(TaalAccounts, data);
+    res.send({msg:"Account registered successfully"})
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ msg: "An error occurred", error: error.message });
+  }
+})
+
+// getting turbidity
+app.get("/api/taal/getreadings/turbidity", async(req, res)=>{
+    const ref = db.ref("/readings/data/turbidity"); // replace with your path
+
+    // res.send(ref)
+  ref.once("value", (snapshot) => {
+    const data = snapshot.val();
+    res.send(data);
+  }, (error) => {
+    console.error(error);
+    res.status(500).send({msg: "An error occurred"});
+  });
+});
+
+// getting Acidity
+app.get("/api/taal/getreadings/ph", async(req, res)=>{
+  const ref = db.ref("/readings/data/ph"); // replace with your path
+
+  // res.send(ref)
+ref.once("value", (snapshot) => {
+  const data = snapshot.val();
+  res.send(data);
+  },(error) => {
+    console.error(error);
+    res.status(500).send({msg: "An error occurred"});
+  });
+});
+
+// gettin temperature
+app.get("/api/taal/getreadings/temperature", async(req, res)=>{
+  const ref = db.ref("/readings/data/temperature"); // replace with your path
+
+  // res.send(ref)
+ref.once("value", (snapshot) => {
+  const data = snapshot.val();
+  res.send(data);
+  },(error) => {
+    console.error(error);
+    res.status(500).send({msg: "An error occurred"});
+  });
+});
+
+
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
